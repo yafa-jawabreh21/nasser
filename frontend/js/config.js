1f3de593d6309dd005f6d36a744a12d40f51ed0a
yafa-jawabreh21 <130245877+yafa-jawabreh21@users.noreply.github.com>
2025-09-02 20:52:40 +0300

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 // عدّل هذا عند النشر
 window.HARMAH_CONFIG = {
-  API_BASE: "http://localhost:8000" // مثال: https://<your-app-id>.appspot.com
+  API_BASE: "https://nasser-rj5s.onrender.com" // رابط الـ API الحي
 };
